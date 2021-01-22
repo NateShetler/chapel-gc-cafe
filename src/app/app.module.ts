64eba6ae77d6806ba96c13920174147b4d4d91e0
Adam Huffman <adam@cyberhuff.com>
2021-01-22 17:06:24 -0500

    Added Initial Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { MenuComponent } from './menu/menu.component';
+import { MenuItemsComponent } from './menu/menu-items/menu-items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    MenuComponent,
+    MenuItemsComponent
   ],
   imports: [
     BrowserModule,
