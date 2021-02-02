eef6baeaefe6214417c0048d17f16cd66abc7054
cyberhuff <adam@cyberhuff.com>
2021-02-02 15:16:48 -0500

    Added admin component to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
 import { MenuComponent } from './components/menu/menu.component';
 
 const routes: Routes = [
-  { path: 'menu', component: MenuComponent }
+  { path: 'menu', component: MenuComponent },
+  { path: 'admin', component: AdminComponent }
 ];
 
 @NgModule({
