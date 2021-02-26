d8e2da93e5cff20774a498096bb934a488f17b90
NateShetler <nateshetler21@gmail.com>
2021-02-26 16:57:05 -0500

    move menu to a service

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { stringify } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
-import { MenuItem } from './models/MenuItem'
+import { MenuService } from '../../services/menu/menu.service'
+import { MenuItem } from './models/MenuItem';
 
 @Component({
   selector: 'app-menu',
@@ -13,61 +14,11 @@ export class MenuComponent implements OnInit {
   // For all of the menu items
   menuItems:MenuItem[];
 
-  constructor() { }
+  constructor(private menuService:MenuService) { }
 
   ngOnInit(): void {
-
-    // Create the list of menu items (this method of storing the item list will soon be replaced with strapi)
-    this.menuItems = [
-      {
-        item: "Breakfast Sandwhich",
-        price: 1,
-        flavors: ["Sausage, Egg, & Cheese", "Bacon, Egg, & Cheese "],
-        isFood: true
-      },
-      {
-        item: "Cookie",
-        price: 1,
-        flavors: ["Breakfast Cookie", "Double Chocolate", "M&M", "Chocolate Chip", "Reese's Cup", "Sugar", "Oatmeal"],
-        isFood: true
-      },
-      {
-        item: "Breakfast Burrito",
-        price: 1,
-        flavors: ["Egg & Cheese"],
-        isFood: true
-      },
-      {
-        item: "Scone",
-        price: 1,
-        flavors: ["Blueberry", "Cranberry"],
-        isFood: true
-      },
-      {
-        item: "Omelet",
-        price: 1,
-        flavors: ["Egg, Potato, & Cheese"],
-        isFood: true
-      },
-      {
-        item: "Muffin",
-        price: 1,
-        flavors: ["Double Chocolate Chip", "Apple Pecan", "Lemon Cranberry", "Blueberry"],
-        isFood: true
-      },
-      {
-        item: "Cheesebox",
-        price: 2,
-        flavors: ["Cheese Cubes, Grapes, & Crackers"],
-        isFood: true
-      },
-      {
-        item: "Donut",
-        price: 1,
-        flavors: ["Glazed", "Cream Stick"],
-        isFood: true
-      }
-    ]
+    this.menuItems = this.menuService.getMenuItems();
+    
   }
 
 }
---
+++ src/app/services/menu/menu.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MenuService {
+
+  menuUrl: 'https://api.thechapel.green/cafe-menus';
+
+  constructor(private http:HttpClient) { }
+
+  getMenuItems()
+  {
+
+    return [
+      {
+        item: "Breakfast Sandwhich",
+        price: 1,
+        flavors: ["Sausage, Egg, & Cheese", "Bacon, Egg, & Cheese "],
+        isFood: true
+      },
+      {
+        item: "Cookie",
+        price: 1,
+        flavors: ["Breakfast Cookie", "Double Chocolate", "M&M", "Chocolate Chip", "Reese's Cup", "Sugar", "Oatmeal"],
+        isFood: true
+      },
+      {
+        item: "Breakfast Burrito",
+        price: 1,
+        flavors: ["Egg & Cheese"],
+        isFood: true
+      },
+      {
+        item: "Scone",
+        price: 1,
+        flavors: ["Blueberry", "Cranberry"],
+        isFood: true
+      },
+      {
+        item: "Omelet",
+        price: 1,
+        flavors: ["Egg, Potato, & Cheese"],
+        isFood: true
+      },
+      {
+        item: "Muffin",
+        price: 1,
+        flavors: ["Double Chocolate Chip", "Apple Pecan", "Lemon Cranberry", "Blueberry"],
+        isFood: true
+      },
+      {
+        item: "Cheesebox",
+        price: 2,
+        flavors: ["Cheese Cubes, Grapes, & Crackers"],
+        isFood: true
+      },
+      {
+        item: "Donut",
+        price: 1,
+        flavors: ["Glazed", "Cream Stick"],
+        isFood: true
+      }
+    ]
+    
+  }
+}
